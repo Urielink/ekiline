a9d2eef0909cd2b828c96edd4c35e8900a5d0e3b
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-02-03 22:06:04 -0600

    replanteo de imagen y meta

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -11,43 +11,40 @@
 
 <article <?php post_class(); ?>>
 
-	<header class="border-bottom pb-2">
+	<header class="border-bottom mb-3">
+
+		<?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
 
 		<?php // Page & post title. ?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<p class="entry-meta small m-0">
-			<?php
-				/* translators: %s is replaced with author link */
-				printf( esc_html_x( 'Written by %s', 'post authors', 'ekiline' ), wp_kses_post( get_the_author_posts_link() ) );
-			?>
-			<br>
+		<p class="entry-meta">
 			<?php
 				/* translators: %s is replaced with post date */
 				printf( esc_html_x( 'Posted on %s', 'post date', 'ekiline' ), wp_kses_post( '<a href="' . get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ) . '" rel="bookmark">' . get_the_time( __( 'F j, Y', 'ekiline' ) ) . '</a>' ) );
 			?>
-			<br>
 			<?php
 				/* translators: %s is replaced with modification date */
 				printf( esc_html__( 'Updated on %s', 'ekiline' ), wp_kses_post( get_the_modified_date( __( 'F j, Y', 'ekiline' ) ) ) );
 			?>
-			<br>
 			<?php
 				printf( wp_kses_post( comments_popup_link( __( 'No comments yet', 'ekiline' ), __( '1 Comment', 'ekiline' ), __( '% Comments', 'ekiline' ), 'comments-link', __( 'Comments are closed.', 'ekiline' ) ) ) );
 			?>
-
 		</p><!-- .entry-meta -->
 
 	</header>
-
-	<?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
 
 	<?php the_content(); ?>
 
 	<?php ekiline_link_pages(); ?>
 
-	<footer class="border-top pt-2">
-		<p class="entry-meta small m-0">
+	<footer class="border-top border-bottom mb-3">
+		<p class="entry-meta my-3">
+			<?php
+				/* translators: %s is replaced with author link */
+				printf( esc_html_x( 'Written by: %s', 'post authors', 'ekiline' ), wp_kses_post( get_the_author_posts_link() ) );
+			?>
+			<br>
 			<?php
 			if ( ! is_page() || get_the_category_list() !== '' ) {
 				// Post format.
