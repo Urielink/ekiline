a66c77c32b465524711b540852293f07c7fc713b
Ricardo Pineda Lazcano <urielink81@gmail.com>
2022-03-19 21:50:31 -0600

    nuevo formato content-search.php

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,18 +9,22 @@
 
 ?>
 
-<article class="bg-white border rounded p-2 mb-2 media">
+<article class="d-flex border-bottom pt-3">
 
-	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-		<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-thumbnail mr-3' ) ); ?>
-	</a>
-
-	<div class="media-body">
+	<div class="flex-grow-1">
 
 		<?php the_title( '<h2 class="entry-title mb-0"><a href="' . get_the_permalink() . '" title="' . get_the_title() . '">', '</a></h2>' ); ?>
 
 		<?php the_excerpt(); ?>
 
 	</div>
+
+	<?php if ( has_post_thumbnail() ){ ?>
+
+		<a class="flex-shrink-0" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-thumbnail img-fluid border-0' ) ); ?>
+		</a>
+
+	<?php } ?>
 
 </article><!-- #post-## -->
